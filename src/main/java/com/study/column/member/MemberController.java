3be03a5437c0a1033ea05caed87ca2d4a4371729
pinwormmy <ozkhan@naver.com>
2023-03-07 02:09:36 +0900

    캐시 삭제하고 깃허브 파일 다 취소되서 다시 잡아 올리기 ㅡㅡ;
    로컬 설정 제외된건 확인. 다시 파일선택해서 커밋
    
    - 승인게시판에 IP로그 기능 추가하기

---
+++ src/main/java/com/study/column/member/MemberController.java
@@ -0,0 +1,108 @@
+package com.study.column.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+public class MemberController {
+    @Autowired
+    MemberService memberService;
+    
+    // 승인 게시판에 아이피 로그 기능 만들기
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request) {
+        // 로그인 이전 페이지 정보 세션에 저장
+        request.getSession().setAttribute("pageBeforeLogin", request.getHeader("Referer"));
+        return "login";
+    }
+
+    @RequestMapping("/signUp")
+    public String signUp() {
+        return "signUp";
+    }
+
+    @RequestMapping("/isUniqueId")
+    @ResponseBody
+    public String isUniqueId(String id) throws Exception {
+        log.debug("(중복확인용)ID 입력 확인: {}", id);
+        return memberService.isUniqueId(id);
+    }
+
+    @RequestMapping(value = "/submitSignUp", method = RequestMethod.POST)
+    public String submitSignUp(MemberDTO memberDTO, HttpSession httpSession) throws Exception {
+        memberService.submitSignUp(memberDTO);
+        httpSession.setAttribute("member", memberService.checkLoginData(memberDTO)); // 로그인도 해줌
+        log.debug("회원가입 확인: {}", memberDTO);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/submitLogin", method = RequestMethod.POST)
+    public String submitLogin(HttpSession session, MemberDTO memberDTO) throws Exception {  
+        session.setAttribute("member", memberService.checkLoginData(memberDTO));
+        log.debug("로그인 확인: {}", memberDTO);
+        return "redirect:" + session.getAttribute("pageBeforeLogin");
+    }
+
+    @RequestMapping(value = "/logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.invalidate();
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/myPage")
+    public String myPage() {
+        return "myPage";
+    }
+
+    @RequestMapping(value = "/modifyMyInfo")
+    public String modifyMyInfo() {
+        return "modifyMyInfo";
+    }
+
+    @RequestMapping(value = "/submitModifyMyInfo", method = RequestMethod.POST)
+    public String submitModifyMyInfo(MemberDTO member, HttpSession session) throws Exception {
+        memberService.submitModifyMyInfo(member);       
+        session.setAttribute("member", memberService.checkLoginData(member)); // 재로그인해서 회원정보갱신
+        return "myPage";
+    }
+
+    @RequestMapping(value = "/modifyMember")
+    public String modifyMember() {
+        return "modifyMember";
+    }
+
+    @RequestMapping(value = "/loginForDemo")
+    public String loginForDemo(HttpServletRequest request) throws Exception {
+        request.getSession().setAttribute("pageBeforeLogin", request.getHeader("Referer"));
+        MemberDTO member = new MemberDTO();
+        member.setId("midori"); // midori는 시연용 일반계정입니다.
+        member.setPw("1111");
+        MemberDTO loginData = memberService.checkLoginData(member);
+        HttpSession session = request.getSession();
+        session.setAttribute("member", loginData);
+        log.info("(시연용) 일반로그인 성공 : {}", loginData);
+        return "redirect:" + session.getAttribute("pageBeforeLogin");
+    }
+
+    @RequestMapping(value = "/loginForDemoAsAdmin")
+    public String loginForDemoAsAdmin(HttpServletRequest request) throws Exception {
+        request.getSession().setAttribute("pageBeforeLogin", request.getHeader("Referer"));
+        MemberDTO member = new MemberDTO();
+        member.setId("admin"); // admin은 관리자계정입니다.
+        member.setPw("1111");
+        MemberDTO loginData = memberService.checkLoginData(member);
+        HttpSession session = request.getSession();
+        session.setAttribute("member", loginData);
+        log.info("(시연용) 관리자로그인 성공 : {}", loginData);
+        return "redirect:" + session.getAttribute("pageBeforeLogin");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/modifyMyInfo.jsp
@@ -0,0 +1,107 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<title>테스트 로그인</title>
+</head>
+<body>
+<%@include file="./include/header.jspf" %>
+
+<header id="headerwrap" class="halfscreen">
+    <div class="align-bottom wow fadeInUp">
+        <div class="row">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="post-heading mb">
+                        <h1>회원정보수정</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="white section-wrapper">
+    <div class="section-inner">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="row">
+                        <div id="comments" class="col-xs-12">
+                            <div id="comments-list" class="gap">
+                                <div class="media">
+                                    <div class="media-body">
+                                        <div class="well">
+                                            <div class="media-heading">
+                                                <strong>${member.nickName}님의 회원정보수정</strong>
+                                            </div>
+                                            <hr>
+                                            <form action="/submitModifyMyInfo" id="modifyMyInfo" method="post">
+                                                <div>
+                                                    <p>ID : ${member.id}</p>
+                                                    <input type="hidden" name="id" value="${member.id}">
+                                                    <p>별명 : <input type="text" name="nickName" value="${member.nickName}"></p>
+                                                    <p>비밀번호 : <input type="password" name="pw" value="${member.pw}"></p>
+                                                    <p>비밀번호확인 : <input type="password" name="pw2" value="${member.pw}"></p>
+                                                    <p>이메일 : <input type="email" name="email" value="${member.email}"></p>
+                                                    <button type="button" class="pull-left btn btn-theme" onclick="checkSignupForm();">수정하기</button>
+                                                    <button type="button" class="pull-left btn btn-theme" onclick="location.href='/myPage'">취소</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script>
+
+    // alert("js test02");
+
+    let modifyMyInfoForm = document.getElementById("modifyMyInfo");
+	let checkUniqueId = false;
+	let checkUniqueNickname = false;
+
+	function checkSignupForm() {
+
+		let isPassword = /\S{4,}/;
+
+		if (modifyMyInfoForm.pw.value == "") {
+			alert("비밀번호를 입력하세요!!");
+			modifyMyInfoForm.pw.focus();
+			return false;
+		}
+		if(!isPassword.test(modifyMyInfoForm.pw.value)){
+			alert("비밀번호는 4자리 이상이어야 합니다;");
+			modifyMyInfoForm.pw.focus();
+			return false;
+		}
+		if (modifyMyInfoForm.pw2.value == "") {
+			alert("비밀번호 확인도 입력하세요!!");
+			modifyMyInfoForm.pw2.focus();
+			return false;
+		}
+		if (modifyMyInfoForm.pw.value != modifyMyInfoForm.pw2.value) {
+			alert("비밀번호 재입력까지 일치해야합니다.");
+			modifyMyInfoForm.pw.focus();
+			return false;
+		}
+		if (modifyMyInfoForm.nickName.value == "") {
+            alert("닉네임을 입력하세요!!");
+            modifyMyInfoForm.nickName.focus();
+            return false;
+        }
+		alert("수정되었습니다.");
+		modifyMyInfoForm.submit();
+	}
+
+</script>
+
+<%@include file="./include/footer.jspf" %>
+</body>
+</html>

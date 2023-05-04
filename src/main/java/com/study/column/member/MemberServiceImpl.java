c287d3ee2df2801d78c3b8dbdeb5e2180fe58060
pinwormmy <ozkhan@naver.com>
2023-05-04 23:48:00 +0900

    회원목록에 페이징+검색 기능 적용 중...
    게시판에서 썼던 로직 그대로 적용 중. 서비스 구현부 수정 중...
    
    - 관리자 권한: 회원 정보 수정 및 탈퇴 구현하기
    - 아이콘, 썸네일 관련 자료 찾아서 도입하기

--- src/main/java/com/study/column/member/MemberServiceImpl.java
+++ src/main/java/com/study/column/member/MemberServiceImpl.java
@@ -2,12 +2,15 @@ package com.study.column.member;
 
 import com.study.column.mapper.MemberMapper;
 import com.study.column.util.PageDTO;
+import com.study.column.util.PageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class MemberServiceImpl implements MemberService {
 
     @Autowired
@@ -45,7 +48,33 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public PageDTO pageSetting(PageDTO page) throws Exception {
-        // 게시판에서 사용한 로직을 참고하여 페이지 설정 로직을 구현합니다.
+        checkPageAndKeyword(page);
+        return utilLoadingForPage(page);
+    }
+
+    private void checkPageAndKeyword(PageDTO page) {
+        if(page.getRecentPage() < 1) { page.setRecentPage(1); }
+        if(page.getSearchType() == null) { page.setSearchType("title"); }
+        if(page.getKeyword() == null) { page.setKeyword(""); }
+    }
+
+    private PageDTO utilLoadingForPage(PageDTO page) throws Exception {
+        log.debug("서비스단계에서 검색어 확인 : {}", page.getKeyword());
+        page.setTotalPostCount(getTotalMemberCount(page));
+        PageService util = initPageUtil();
+        return util.calculatePage(page);
+    }
+
+    @Override
+    public int getTotalMemberCount(PageDTO page) throws Exception {
+        // 게시판에서 사용한 로직을 참고하여 전체 회원 수 조회 로직을 구현합니다.
+    }
+
+    private PageService initPageUtil() {
+        PageService util = new PageService();
+        util.setDISPLAY_POST_LIMIT(10);
+        util.setPAGESET_LIMIT(10);
+        return util;
     }
 
     @Override
@@ -53,8 +82,5 @@ public class MemberServiceImpl implements MemberService {
         // 게시판에서 사용한 로직을 참고하여 회원 목록 조회 로직을 구현합니다.
     }
 
-    @Override
-    public int getTotalMemberCount(PageDTO page) throws Exception {
-        // 게시판에서 사용한 로직을 참고하여 전체 회원 수 조회 로직을 구현합니다.
-    }
+
 }

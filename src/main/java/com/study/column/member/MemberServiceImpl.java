f70138bac1c9ad5dbc8f4a8e9a556e80524ac2fe
pinwormmy <ozkhan@naver.com>
2023-05-06 00:21:15 +0900

    (알바뛰고, 학점은행제 시험까지 치느라, 코딩 못함 ㅋ)
    회원목록에 페이징+검색 기능 적용 중...
    
    게시판에서 썼던 로직 그대로 적용 중. 서비스 구현부 수정 중...
    
    - 관리자 권한: 회원 정보 수정 및 탈퇴 구현하기
    - 아이콘, 썸네일 관련 자료 찾아서 도입하기

--- src/main/java/com/study/column/member/MemberServiceImpl.java
+++ src/main/java/com/study/column/member/MemberServiceImpl.java
@@ -67,7 +67,7 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public int getTotalMemberCount(PageDTO page) throws Exception {
-        // 게시판에서 사용한 로직을 참고하여 전체 회원 수 조회 로직을 구현합니다.
+        // 게시판에 서비스 로직 참고 및 수정해서 가져오기
     }
 
     private PageService initPageUtil() {
@@ -79,7 +79,7 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public List<MemberDTO> getMemberList(PageDTO page) throws Exception {
-        // 게시판에서 사용한 로직을 참고하여 회원 목록 조회 로직을 구현합니다.
+        // 실제 필요 기능
     }
 
 
